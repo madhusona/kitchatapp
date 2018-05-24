2b64edda6568ae55fdcd8677273d3241600d4dfa
Madusudhanan <madhusona@gmail.com>
2018-05-24 12:25:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express');
   var app = express();
   var server = require('http').createServer(app);
   var io = require('socket.io')(server);
-
+  var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+  var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
   app.get('/', function(req, res, next) {
   	res.sendFile(__dirname + '/public/index.html')
   });
@@ -22,4 +23,8 @@ client.on('messages', function(data){
   	});
   });
 
-  server.listen(7777);
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
+
+  //server.listen(7777);
