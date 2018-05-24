d7cd75404d288181d0481ed276870db510933c54
Madusudhanan <madhusona@gmail.com>
2018-05-24 13:51:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
   var app = express();
   var server = require('http').createServer(app);
   var io = require('socket.io')(server);
-  var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+  var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8000
   var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
   app.get('/', function(req, res, next) {
   	res.sendFile(__dirname + '/public/index.html')
